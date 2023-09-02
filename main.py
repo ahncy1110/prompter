c52e54495014e29e59500b638cf02b734a3b1ac7
ahncy1110 <97164267+ahncy1110@users.noreply.github.com>
2023-09-02 14:34:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,32 +11,30 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from PIL import Image
+import time
+
+#파일 업로드
+# ["samsung_tv_manual.pdf", "lg_ac_manual.pdf", "winix_humidifier_manual.pdf"]
+tv_file = PyPDFLoader("samsung_tv_manual.pdf")
+ac_file = PyPDFLoader("lg_ac_manual.pdf")
+hm_file = PyPDFLoader("winix_humidifier_manual.pdf")
 
 #제목
 st.title("ChatPDF")
 st.write("---")
 
-#파일 업로드
-uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
+# 방 이미지
+cyworld_img = Image.open('cyworld-room.jpg')
+st.image(cyworld_img)
 st.write("---")
 
-def pdf_to_document(uploaded_file):
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
-    with open(temp_filepath, "wb") as f:
-        f.write(uploaded_file.getvalue())
-    loader = PyPDFLoader(temp_filepath)
-    pages = loader.load_and_split()
-    return pages
-
-#업로드 되면 동작하는 코드
-if uploaded_file is not None:
-    pages = pdf_to_document(uploaded_file)
-
+def document_to_db(uploaded_file, size):    # 문서 크기에 맞게 사이즈 지정하면 좋을 것 같아서 para 넣었어용
+    pages = uploaded_file.load_and_split()
     #Split
     text_splitter = RecursiveCharacterTextSplitter(
         # Set a really small chunk size, just to show.
-        chunk_size = 300,
+        chunk_size = size,
         chunk_overlap  = 20,
         length_function = len,
         is_separator_regex = False,
@@ -48,14 +46,53 @@ if uploaded_file is not None:
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
+    return db
 
-    #Question
-    st.header("PDF에게 질문해보세요!!")
-    question = st.text_input('질문을 입력하세요')
+#업로드 되면 동작하는 코드
+if tv_file is not None:
+    db_tv = document_to_db(tv_file, 500)
+    db_ac = document_to_db(ac_file, 500)
+    db_hm = document_to_db(hm_file, 300)
 
-    if st.button('질문하기'):
-        with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+    #Choice
+    st.header("기기를 선택하고 PDF에게 질문해보세요!!")
+    menu = ['TV', '에어컨', '가습기']    #options
+    # choice_box = st.radio('기기를 선택하세요', menu)
+    # st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)    
+    choice_box = st.selectbox('기기를 선택하세요', menu)
+    
+    if choice_box == menu[0]:
+        tv_img = Image.open('television.png')
+        tv_img = tv_img.resize((100, 100))
+        st.image(tv_img)
+        question = st.text_input('질문을 입력하세요')
+        if st.button('TV에게 질문하기'):
+            with st.spinner('Wait for it...'):
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+                qa_chain = RetrievalQA.from_chain_type(llm,retriever=db_tv.as_retriever())
+                result = qa_chain({"query": question})
+                st.write(result["result"])
+                
+    elif choice_box == menu[1]:
+        ac_img = Image.open('air-conditioner.png')
+        ac_img = ac_img.resize((100, 100))
+        st.image(ac_img)
+        question = st.text_input('질문을 입력하세요')
+        if st.button('에어컨에게 질문하기'):
+            with st.spinner('Wait for it...'):
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+                qa_chain = RetrievalQA.from_chain_type(llm,retriever=db_ac.as_retriever())
+                result = qa_chain({"query": question})
+                st.write(result["result"])
+
+    elif choice_box == menu[2]:
+        hm_img = Image.open('humidifier.png')
+        hm_img = hm_img.resize((100, 100))
+        st.image(hm_img)
+        question = st.text_input('질문을 입력하세요')
+        if st.button('가습기에게 질문하기'):
+            with st.spinner('Wait for it...'):
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+                qa_chain = RetrievalQA.from_chain_type(llm,retriever=db_hm.as_retriever())
+                result = qa_chain({"query": question})
+                st.write(result["result"])
